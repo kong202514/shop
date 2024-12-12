fd1f6fdcdcaae7bb388ba1b24c5ee3103c4a7001
kong202514 <86356566+kong202514@users.noreply.github.com>
2024-12-12 23:24:26 +0700

    asd

---
+++ header.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="index.css"> -->
+
+</head>
+
+<body class="body">
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">logo</a>
+
+            <div class="collapse navbar-collapse" id="navbarsExample03">
+                <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+
+                    <!-- เมนู  Home  -->
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <!-- เมนู   workout  -->
+                    <li class="nav-item">
+                        <a class="nav-link  active" href="#" aria-disabled="true">workout </a>
+                    </li>
+
+                    <!-- เมนู   terminal  -->
+                    <li class="nav-item">
+                        <a class="nav-link  active" href="#" aria-disabled="true">terminal </a>
+                    </li>
+
+
+                    <!-- เมนู   test input text to firebase  -->
+                    <li class="nav-item">
+                        <a class="nav-link active  " href="#">
+                            test input text to firebase </a>
+                    </li>
+
+
+
+                    <!-- เมนู   color  -->
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">
+                            color </a>
+                    </li>
+
+
+                    <!-- เมนู   tool  -->
+                    <li class="nav-item">
+                        <a class="nav-link  active" href="#" aria-disabled="true">tool</a>
+                    </li>
+
+
+
+                    <!-- เมนู   เมนูอื่นๆ  -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            เมนูอื่นๆ
+                        </a>
+
+                        <!-- เมนู เมนูอื่นๆ  >>> dropdown  -->
+
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+                <form role="search">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
+
+</html>
